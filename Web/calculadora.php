d29fdc2419e04169cc6860be99c9337146107bc2
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-05-14 14:42:03 -0600

    Agregando etiquetas en el archivo calculadora

--- Web/calculadora.php
+++ Web/calculadora.php
@@ -1,28 +1,39 @@
 <?php session_start(); ?>
-<?php 
+<?php
 $conn = include "conexion/conexion.php";
 
-if(isset($_GET['fecha'])){
-$fecha_consultar = $_GET['fecha'];
-}else{
-date_default_timezone_set('US/Central');  
-$fecha_consultar = date("Y-m-d");
+if (isset($_GET['fecha'])) {
+    $fecha_consultar = $_GET['fecha'];
+} else {
+    date_default_timezone_set('US/Central');
+    $fecha_consultar = date("Y-m-d");
 }
 
 $nahual = include 'backend/buscar/conseguir_nahual_nombre.php';
 $energia = include 'backend/buscar/conseguir_energia_numero.php';
 $haab = include 'backend/buscar/conseguir_uinal_nombre.php';
 $cuenta_larga = include 'backend/buscar/conseguir_fecha_cuenta_larga.php';
-$cholquij = $nahual." ". strval($energia);
+$cholquij = $nahual . " " . strval($energia);
 
 ?>
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <title>Tiempo Maya - Calculadora de Mayas</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="description" content="Web que difunde conocimientos sobre la cultura Maya, como sus calendarios, el conteo del tiempo, los Nahuales y las energías. Su objetivo es despertar interés en la cultura Maya y facilitar la interacción con el usuario, sirviendo como canal de comunicación y construcción del conocimiento para quienes estén interesados.">
+    <meta name="keywords" content="Cholqij, Habb, Rueda Calendárica, Nahual, energias, kines, uniales, nahuales, tiempo, maya, tzolkin, cuenta larga">
+    <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+    <meta property="og:image" content="http://localhost/tiempo-maya/img/tiempo-maya.png">
+    <meta property="og:title" content="Tiempo Maya">
+    <meta property="og:description" content="Web que difunde conocimientos sobre la cultura Maya, como sus calendarios, el conteo del tiempo, los Nahuales y las energías. Su objetivo es despertar interés en la cultura Maya y facilitar la interacción con el usuario, sirviendo como canal de comunicación y construcción del conocimiento para quienes estén interesados.">
+    <meta property="og:url" content="https://tiempomaya.com/">
+    <meta property="og:site_name" content="Tiempo Maya">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="es_GT">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon.ico">
     <?php include "blocks/bloquesCss.html" ?>
     <link rel="stylesheet" href="css/estilo.css?v=<?php echo (rand()); ?>" />
     <link rel="stylesheet" href="css/calculadora.css?v=<?php echo (rand()); ?>" />
@@ -40,7 +51,7 @@ $cholquij = $nahual." ". strval($energia);
                     <form action="#" method="GET">
                         <div class="mb-1">
                             <label for="fecha" class="form-label">Fecha</label>
-                            <input type="date" class="form-control" name="fecha" id="fecha" value="<?php echo isset($fecha_consultar) ? $fecha_consultar : ''; ?>" >
+                            <input type="date" class="form-control" name="fecha" id="fecha" value="<?php echo isset($fecha_consultar) ? $fecha_consultar : ''; ?>">
                         </div>
                         <button type="submit" class="btn btn-get-started"><i class="far fa-clock"></i> Calcular</button>
                     </form>
@@ -57,7 +68,7 @@ $cholquij = $nahual." ". strval($energia);
                             <tbody>
                                 <tr>
                                     <th scope="row">Calendario Haab</th>
-                                    <td ><?php echo isset($haab) ? $haab : ''; ?></td>
+                                    <td><?php echo isset($haab) ? $haab : ''; ?></td>
                                 </tr>
                                 <tr>
                                     <th scope="row">Calendario Cholquij</th>
