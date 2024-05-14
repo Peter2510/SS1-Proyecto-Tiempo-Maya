a3cf7556e8ac1a7fd6a839f165964e701d658dff
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-05-14 14:40:04 -0600

    Cambio navbar alineado, manejo CEO, y fecha index

--- Web/index.php
+++ Web/index.php
@@ -5,7 +5,7 @@ if (isset($_GET['fecha'])) {
   $fecha_consultar = $_GET['fecha'];
 } else {
   date_default_timezone_set('US/Central');
-  $fecha_consultar = date("Y-m-d");
+  $fecha_consultar = date("d-m-Y");
 }
 
 $nahual = include 'backend/buscar/conseguir_nahual_nombre.php';
@@ -24,7 +24,7 @@ $cholquij = $nahual . " " . strval($energia);
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta name="description" content="Web que difunde conocimientos sobre la cultura Maya, como sus calendarios, el conteo del tiempo, los Nahuales y las energías. Su objetivo es despertar interés en la cultura Maya y facilitar la interacción con el usuario, sirviendo como canal de comunicación y construcción del conocimiento para quienes estén interesados.">
   <meta name="keywords" content="Cholqij, Habb, Rueda Calendárica, Nahual, energias, kines, uniales, nahuales, tiempo, maya, tzolkin, cuenta larga">
-  <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
   <meta property="og:image" content="http://localhost/tiempo-maya/img/tiempo-maya.png">
   <meta property="og:title" content="Tiempo Maya">
   <meta property="og:description" content="Web que difunde conocimientos sobre la cultura Maya, como sus calendarios, el conteo del tiempo, los Nahuales y las energías. Su objetivo es despertar interés en la cultura Maya y facilitar la interacción con el usuario, sirviendo como canal de comunicación y construcción del conocimiento para quienes estén interesados.">
@@ -37,7 +37,6 @@ $cholquij = $nahual . " " . strval($energia);
 
   <link rel="stylesheet" href="css/estilo.css?v=<?php echo (rand()); ?>" />
   <link rel="stylesheet" href="css/estiloAdmin.css?v=<?php echo (rand()); ?>" />
-
   <link rel="stylesheet" href="css/index.css?v=<?php echo (rand()); ?>" />
 
 
@@ -46,20 +45,27 @@ $cholquij = $nahual . " " . strval($energia);
 <body>
 
   <?php include "NavBar.php" ?>
-  <div>
-    <section id="inicio">
-      <div id="inicioContainer" class="inicio-container">
-        <h1><br><br>Bienvenido al Tiempo Maya</h1>
-        <div id='formulario' style="padding: 15px; width: auto;">
-          <h5 style="color: whitesmoke;">Calendario Haab : <?php echo isset($haab) ? $haab : ''; ?></h5>
-          <h5 style="color: whitesmoke;">Calendario Cholquij : <?php echo isset($cholquij) ? $cholquij : ''; ?></h5>
-          <h5 style="color: whitesmoke;">Cuenta Larga : <?php echo isset($cuenta_larga) ? $cuenta_larga : ''; ?></h5>
-          <label style="color: whitesmoke;"><?php echo isset($fecha_consultar) ? $fecha_consultar : ''; ?></label>
-        </div>
-      </div>
-    </section>
-  </div>
 
+  <div>
+
+  <section id="inicio" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+  <div id="inicioContainer" class="inicio-container" style="text-align: center;">
+    <h1><br><br>Bienvenido al Tiempo Maya</h1>
+    <div id='texto' style="padding: 15px; width: auto; max-width: 800px; margin: 0 auto;">
+      <p style="color: whitesmoke; text-align: center; font-size: 1.2rem;">
+        Aquí podrás explorar los diversos calendarios mayas, entender cómo contaban el tiempo, aprender sobre los nahuales y las energías que los mayas consideraban importantes.
+      </p>
+    </div>
+    <div id='formulario' style="padding: 15px; width: auto;">
+      <h5 style="color: whitesmoke;">Calendario Haab : <?php echo isset($haab) ? $haab : ''; ?></h5>
+      <h5 style="color: whitesmoke;">Calendario Cholquij : <?php echo isset($cholquij) ? $cholquij : ''; ?></h5>
+      <h5 style="color: whitesmoke;">Cuenta Larga : <?php echo isset($cuenta_larga) ? $cuenta_larga : ''; ?></h5>
+      <label style="color: whitesmoke;"><?php echo isset($fecha_consultar) ? $fecha_consultar : ''; ?></label>
+    </div>
+  </div>
+</section>
+
+  </div>
 
   <?php include "blocks/bloquesJs1.html" ?>
 
